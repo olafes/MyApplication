e09a8d71ab4b66eb89c386aee157a58b6788e8b1
Olaf Siwinski <olafsiwinski@gmail.com>
2019-10-14 18:35:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/fragments/SignInFragment.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication.fragments
+
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_sign_in.*
+
+class SignInFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+// Load the animation like this
+
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment SignInFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            SignInFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
